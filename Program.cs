b0ac7cb2b922f09b5d0d15da691f9d3c64eba85e
Sam Sully <sam@planetaryprocessing.io>
2024-01-25 15:54:01 +0000

    use socket not tcpclient
    Change-Id: Ic3b7735ad684ddced14440473b3f6df1a1ff1abf

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Planetary;
 StreamReader r = new StreamReader(Environment.GetEnvironmentVariable("HOME") + "/.go-easyops/tokens/user_token");
 var token = r.ReadLine();
 
-var sdk = new SDK(1);
+var sdk = new SDK(73);
 sdk.Connect(token);
 while (sdk.IsConnected()) {
   Thread.Sleep(1000/60);
