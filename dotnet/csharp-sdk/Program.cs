61cf446e9822e6f93ce0ca77883e3cf242bd2413
Matt <matt@planetaryprocessing.io>
2025-04-02 15:33:55 +0100

    websocket update

--- dotnet/csharp-sdk/Program.cs
+++ dotnet/csharp-sdk/Program.cs
@@ -1,13 +1,12 @@
 using Planetary;
 
-StreamReader r = new StreamReader(Environment.GetEnvironmentVariable("HOME") + "/.go-easyops/tokens/user_token");
-var token = r.ReadLine();
 
-var sdk = new SDK(1);
-sdk.Connect("test", "testsdafasdfsda");
+var sdk = new SDK(7922);
+await sdk.Connect("1", "1");
+sdk.Join();
 while (sdk.IsConnected()) {
     for (int i = 0; i < 5; i++) {
-        sdk.Update();
+        //sdk.Update();
         Thread.Sleep(1000/5);        
     }
     sdk.Message(new Dictionary<string, dynamic>());
